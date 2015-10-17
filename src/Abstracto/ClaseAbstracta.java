4b28a0f040ef19920b2c88138ae7d54cb8122b00
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-10-17 11:41:14 -0500

    Crea la clase abstracta "ClaseAbstracta"
     - Crea el método "metodoNormal()" que no retorna valor alguno e
       imprime un mensaje.
     - Crea el método abstracto "metodoAbstracto()" se agrega a este
       después de su modificador de acceso la palabra reservada "abstract"
       que indica que dicho método es abstracto. Este no posee cuerpo
       debido a que así debe crearse para que sea abstracto.
     - Agrega a la clase después de su modificador de acceso la palabra
       reservada "abstract" que indica que dicha clase es ahora abstracta

---
+++ src/Abstracto/ClaseAbstracta.java
@@ -0,0 +1,16 @@
+package Abstrato;
+
+public abstract class ClaseAbstracta {
+	
+	//-> Método normal
+	public void metodoNormal() {
+		System .out .println( "Soy un método normal" );
+	}
+	
+	//-> Método Abstracto
+	//   Todo método de este tipo obliga a que la clase que lo contienen sea abstracta.
+	//   Los métodos abstractos no poseen cuerpo
+	public abstract void metodoAbstracto();
+	
+
+}
