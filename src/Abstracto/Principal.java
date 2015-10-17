de13b4acf3fcb6cdc0e8f26ae8b20a348be167de
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-10-17 12:11:58 -0500

    Crea la clase "Principal" e instancia "ClaseHija1"
     - Instancia "ClaseHija1"
     - Llama al método "metodoNormal()" que heredó de "ClaseAbstracta"
     - Llama el método "metodoAbstracto()" que heredó de "ClaseAbstracta"

---
+++ src/Abstracto/Principal.java
@@ -0,0 +1,14 @@
+package Abstracto;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		ClaseHija1 ch1 = new ClaseHija1();
+		ch1 .metodoNormal();
+		ch1 .metodoAbstracto();
+		
+	}
+
+}
